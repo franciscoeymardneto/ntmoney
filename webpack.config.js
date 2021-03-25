d6b5f8c57a506c16c3e61865c2aba927548df34c
Eymard Neto <francisco.eymard.oliveira@gmail.com>
2021-03-24 21:41:42 -0300

    chore: add html webpack plugin and react refresh webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
 const path = require('path')
+const HtmlwebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'producttion',
+
   entry: path.resolve(__dirname,'src/index.ts'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
 
-  resolve: ['.js','.jsx','.ts','.tsx'],
+  resolve: {
+    extensions: ['.js','.jsx','.ts','.tsx']
+  },
 
   devServer: {
     contentBase: path.resolve(__dirname,'public'),
     hot: true
   },
+
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
 
   module: {
     rules: [
