b9d088a41d9b3dc4676d989012458b8d873692e1
Eymard Neto <francisco.eymard.oliveira@gmail.com>
2021-03-29 20:46:33 -0300

    feat: create miragejs server

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { GlobalStyle } from './assets/styles/global';
 import { Dashboard } from './components/Dashboard/Dashboard';
 import { Header } from './components/Header';
-import {} from 
 export function App() {
   return (
     <>
