a150190cd449efd3c97c702bfa32422b4e8c655d
Eymard Neto <francisco.eymard.oliveira@gmail.com>
2021-03-29 21:03:04 -0300

    feat: add axios config

--- src/components/TransactionsTable/TransactionsTable.tsx
+++ src/components/TransactionsTable/TransactionsTable.tsx
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
+import { api } from '../../services/api';
 import { Container } from './styles';
 
 export function TransactionsTable(){
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/transactions')
-    .then(response => response.json())
-    .then(data => console.log(data));
+    api.get('http://localhost:8080/api/transactions')
+    .then(response => console.log(response.data));
   },[])
 
   return(
